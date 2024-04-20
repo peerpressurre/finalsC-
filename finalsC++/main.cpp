f236ad090eb3b43b3e547d794c24ba2f82c98804
peerpressurre <gsophiay@gmail.com>
2024-04-20 09:19:08 +0300

    74

--- finalsC++/main.cpp
+++ finalsC++/main.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <iostream>
 #include <fstream>
 #include <map>
@@ -47,53 +48,83 @@ public:
     }
 };
 
-class FileManager {
+
+class Date {
+    std::chrono::system_clock::time_point timePoint;
+
 public:
-    static bool fileExists(const std::string& filename) {
-        std::ifstream file(filename);
-        return file.good();
+    Date() : timePoint(std::chrono::system_clock::now()) {}
+
+    int getYear() const {
+        time_t time = chrono::system_clock::to_time_t(timePoint);
+        tm* localTime = localtime(&time);
+        return localTime->tm_year + 1900;
     }
 
-    static void createWalletFile( std::string& walletName,  Wallet& wallet) {
-        std::ofstream file(walletName + ".txt");
-        if (file.is_open()) {
-            file << "Wallet Name: " << wallet.getName() << "\n\n";
-            for (auto& card : wallet.getCards()) {
-                file << "Card Number: " << card.getNumber() << "\n";
-                file << "Holder Name: " << card.getName() << "\n";
-                file << "Balance: " << card.getBalance() << "\n";
-                file << "Transactions:\n";
-                for (auto& transactionPair : card.getTransactions()) {
-                    auto& category = transactionPair.first;
-                    auto& transactions = transactionPair.second;
+    int getMonth() const {
+        time_t time = chrono::system_clock::to_time_t(timePoint);
+        tm* localTime = localtime(&time);
+        return localTime->tm_mon + 1;
+    }
 
-                    file << category << ":\n";
+    int getDay() const {
+        time_t time = chrono::system_clock::to_time_t(timePoint);
+        tm* localTime = localtime(&time);
+        return localTime->tm_mday;
+    }
 
-                    for (auto& transaction : transactions) {
-                        file << "- " << transaction.getAmount() << "$ " << transaction.getDate() << "\n";
-                    }
-                }
-                file << "\n";
-            }
-        }
-        else {
-            cout << "Failed to create wallet: " << walletName << endl;
-        }
+    int getHour() const {
+        time_t time = chrono::system_clock::to_time_t(timePoint);
+        tm* localTime = localtime(&time);
+        return localTime->tm_hour;
+    }
+
+    int getMinute() const {
+        time_t time = chrono::system_clock::to_time_t(timePoint);
+        tm* localTime = localtime(&time);
+        return localTime->tm_min;
+    }
+
+    int getSecond() const {
+        time_t time = chrono::system_clock::to_time_t(timePoint);
+        tm* localTime = localtime(&time);
+        return localTime->tm_sec;
+    }
+
+    string formatDateTime() const {
+        time_t time = chrono::system_clock::to_time_t(timePoint);
+        tm* localTime = localtime(&time);
+
+        std::ostringstream oss;
+        oss << setfill('0');
+        oss << setw(2) << localTime->tm_mday << ' '
+            << setw(2) << (localTime->tm_mon + 1) << '-'
+            << setw(4) << (localTime->tm_year + 1900) << '-'
+            << setw(2) << localTime->tm_hour << ':'
+            << setw(2) << localTime->tm_min << ':'
+            << setw(2) << localTime->tm_sec;
+
+        return oss.str();
     }
 };
 
 
-class Wallet {
-    string holderName;
-    vector<Card> cards;
-public:
-    Wallet(vector<Card> cardss) : cards(cardss) {};
-    string getName() {
-        return holderName;
-    }
+class Transaction {
+private:
+    string category;
+    double amount;
+    Date date;
 
-    vector<Card>& getCards() {
-        return cards;
+public:
+    Transaction(string cat, double amt) : category(cat), amount(amt) {}
+    string getCategory() {
+        return category;
+    }
+    double getAmount() {
+        return amount;
+    }
+    string getDate() {
+        return date.formatDateTime();
     }
 };
 
@@ -130,84 +161,186 @@ public:
     void deposit(double amount) {
         balance += amount;
     }
-   
+
 };
 
-class Transaction {
-private:
-    string category;
-    double amount;
-    Date date;
 
+class Wallet {
+    string holderName;
+    vector<Card> cards;
 public:
-    Transaction(string cat, double amt) : category(cat), amount(amt) {}
-    string getCategory() {
-        return category;
+    Wallet(string name, vector<Card> cardss) : holderName(name), cards(cardss) {};
+    string getName() {
+        return holderName;
     }
-    double getAmount() {
-        return amount;
-    }
-    string getDate() {
-        return date.formatDateTime();
+
+    vector<Card>& getCards() {
+        return cards;
     }
 };
 
-class Date {
-    std::chrono::system_clock::time_point timePoint;
 
+class FileManager {
 public:
-    Date() : timePoint(std::chrono::system_clock::now()) {}
-
-    int getYear() const {
-        time_t time = chrono::system_clock::to_time_t(timePoint);
-        tm* localTime = localtime(&time);
-        return localTime->tm_year + 1900; 
+    static bool fileExists(const string& filename) {
+        fstream file(filename);
+        return file.good();
     }
 
-    int getMonth() const {
-        time_t time = chrono::system_clock::to_time_t(timePoint);
-        tm* localTime = localtime(&time);
-        return localTime->tm_mon + 1;
+    static void createWalletFile(string& walletName, Wallet& wallet) {
+        fstream file(walletName + ".txt");
+        if (file.is_open()) {
+            file << "Wallet Name: " << wallet.getName() << "\n\n";
+            for (auto& card : wallet.getCards()) {
+                file << "Card Number: " << card.getNumber() << "\n";
+                file << "Holder Name: " << card.getName() << "\n";
+                file << "Balance: " << card.getBalance() << "\n";
+                file << "Transactions:\n";
+                for (auto& transactionPair : card.getTransactions()) {
+                    auto& category = transactionPair.first;
+                    auto& transactions = transactionPair.second;
+
+                    file << category << ":\n";
+
+                    for (auto& transaction : transactions) {
+                        file << "- " << transaction.getAmount() << "$ " << transaction.getDate() << "\n";
+                    }
+                }
+                file << "\n";
+            }
+        }
+        else {
+            cout << "Failed to create a wallet: " << walletName << endl;
+        }
     }
 
-    int getDay() const {
-        time_t time = chrono::system_clock::to_time_t(timePoint);
-        tm* localTime = localtime(&time);
-        return localTime->tm_mday; 
-    }
-
-    int getHour() const {
-        time_t time = chrono::system_clock::to_time_t(timePoint);
-        tm* localTime = localtime(&time);
-        return localTime->tm_hour;
-    }
-
-    int getMinute() const {
-        time_t time = chrono::system_clock::to_time_t(timePoint);
-        tm* localTime = localtime(&time);
-        return localTime->tm_min;
-    }
-
-    int getSecond() const {
-        time_t time = chrono::system_clock::to_time_t(timePoint);
-        tm* localTime = localtime(&time);
-        return localTime->tm_sec; 
-    }
-
-    string formatDateTime() const {
-        time_t time = chrono::system_clock::to_time_t(timePoint);
-        tm* localTime = localtime(&time);
-
-        std::ostringstream oss;
-        oss << setfill('0');
-        oss << setw(2) << localTime->tm_mday << ' '
-            << setw(2) << (localTime->tm_mon + 1) << '-'
-            << setw(4) << (localTime->tm_year + 1900) << '-'
-            << setw(2) << localTime->tm_hour << ':'
-            << setw(2) << localTime->tm_min << ':'
-            << setw(2) << localTime->tm_sec;
-
-        return oss.str();
+    static void listWalletInfo(const string& walletName) {
+        std::ifstream file(walletName + ".txt");
+        if (file.is_open()) {
+            string line;
+            while (getline(file, line)) {
+                if (line.find("Card") != string::npos) {
+                    cout << line << endl; 
+                    while (getline(file, line) && !line.find("Transactions")) {
+                        cout << line << std::endl; 
+                    }
+                    cout << std::endl; 
+                }
+            }
+            file.close();
+        }
+        else {
+            std::cerr << "Failed to open wallet file: " << walletName << std::endl;
+        }
     }
 };
 
+
+int main() {
+    string walletName;
+    cout << "Enter the name of your wallet: ";
+    getline(std::cin, walletName);
+    vector<Card> cards;
+    if (!FileManager::fileExists(walletName + ".txt")) {
+        cout << "No such wallet found" << endl;
+        cout << "Creating a new wallet.." << endl;
+        cout << "name: " << walletName << endl;
+        bool exitLoop = true;
+        char ans;
+        int counter = 0;
+        while (exitLoop)
+        {
+            cout << "Add card? y/n -> ";
+            cin >> ans;
+            cin.ignore();
+            if (ans == 'y')
+            {
+                string number;
+                string holderName;
+                double balance;
+                while (true)
+                {
+                    cout << "Enter card number (16 digits) -> ";
+                    getline(cin, number);
+                    number.erase(remove_if(number.begin(), number.end(), std::isspace), number.end());
+                    if (number.size() != 16) {
+                        cout << "!Card number must consist of 16 digits" << endl;
+                    }
+                    else {
+                        exitLoop = false;
+                        break;
+                    }
+                }
+                cout << "Enter card holder name -> ";
+                getline(cin, holderName);
+                cout << "Enter current balance -> ";
+                cin >> balance;
+                Card card(number, holderName, balance);
+                cards.push_back(card);
+                
+            }
+        }  
+        Wallet wallet(walletName, cards);
+        FileManager::createWalletFile(walletName, wallet);
+    }
+    else {
+        cout << "Wallet file already exists.\n";
+    }
+
+  
+    return 0;
+}
+    /*/ Creating a debit card
+    Card debitCard("123456789", "John Doe", 1000.0);
+    cards.push_back(debitCard);
+
+    // Creating a credit card
+    Card creditCard("987654321", "Jane Smith", 500.0);
+    cards.push_back(creditCard);
+
+    // Creating the wallet
+    Wallet wallet(cards);
+
+    // Saving wallet data to file
+    FileManager::createWalletFile(walletName, wallet);
+
+    // Sample functions
+    // You can add more functions for managing finances here
+
+    // Example: Add expense
+    addExpense(wallet, "groceries", 50.0);
+
+    // Example: Print report for the day
+    printDailyReport(wallet);
+
+    // Example: Print top 3 expenses of the month
+    printTopExpensesOfMonth(wallet);
+
+    // Example: Print top 3 expense categories of the week
+    printTopExpenseCategoriesOfWeek(wallet);
+
+    return 0;
+}
+
+void addExpense(Wallet& wallet, const std::string& category, double amount) {
+    // Assuming the expense is for the first card in the wallet
+    if (!wallet.getCards().empty()) {
+        // Withdraw the amount from the first card with the given category
+        wallet.getCards()[0].withdraw(category, amount);
+    }
+}
+
+void printDailyReport(const Wallet& wallet) {
+    std::cout << "Daily Report:\n";
+    // Implement logic to print daily report
+}
+
+void printTopExpensesOfMonth(const Wallet& wallet) {
+    std::cout << "Top 3 Expenses of the Month:\n";
+    // Implement logic to print top expenses of the month
+}
+
+void printTopExpenseCategoriesOfWeek(const Wallet& wallet) {
+    std::cout << "Top 3 Expense Categories of the Week:\n";
+    // Implement logic to print top expense categories of the week
+}*/
